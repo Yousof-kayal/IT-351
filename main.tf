86fe0e2329af431bffdb39518c79cf1af4c4ef28
Yousof Kayal <yak35097@marymount.edu>
2021-09-24 14:22:52 -0400

    added .tf files

---
+++ main.tf
@@ -0,0 +1,13 @@
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-087c17d1fe0178315"
+  instance_type = "t2.micro"
+
+  root_block_device {
+    volume_size = 8
+  }
+
+  tags = {
+    Name = "YousofsFirstTFInstance"
+  }
+}
