dcda7769a32fe98e5b6ea7c0d935e039d2410e99
Yousof Kayal <yak35097@marymount.edu>
2021-10-02 14:29:47 -0400

    Modified Terraform to create an Apache webpage

--- main.tf
+++ main.tf
@@ -2,12 +2,17 @@
 resource "aws_instance" "my_instance" {
   ami           = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
+  key_name = "devsecops-key"
 
   root_block_device {
     volume_size = 8
   }
 
   tags = {
-    Name = "YousofsFirstTFInstance"
+    Name = "IsThisThingOn"
+
+  }
+  output "instance_ip" {
+    value = aws_instance.my_instance.public_ip
   }
 }
